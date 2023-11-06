05007ddb158d047db9ab7b0eb86466351cede62a
7HAB <hebabackar@gmail.com>
2023-11-06 19:47:47 +0200

    fixed medical history

--- graduationProject.DAL/Repos/Patient/PatientRepo.cs
+++ graduationProject.DAL/Repos/Patient/PatientRepo.cs
@@ -28,8 +28,10 @@ namespace graduationProject.DAL
         public MedicaHistory? GetMedicaHistoryByPhoneNumber(string phoneNumber)
         {
             Patient? patient = _context.Set<Patient>().FirstOrDefault(p => p.PhoneNumber == phoneNumber);
-            if (patient == null) { return null; }
-            return patient.MedicaHistory;
+            MedicaHistory? medicaHistory = _context.Set<MedicaHistory>().FirstOrDefault(m => m.PatientId == patient.Id);
+            if (medicaHistory == null) { return null; }
+
+            return medicaHistory;
         }
 
         #endregion
